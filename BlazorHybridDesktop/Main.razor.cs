146749eb6ba9def9e98ace13aef3188d5547b645
Michael Bond <codemonkey8510@icloud.com>
2021-04-26 10:34:06 -0400

    Try all-webview content

--- BlazorHybridDesktop/Main.razor.cs
+++ BlazorHybridDesktop/Main.razor.cs
@@ -1,7 +1,11 @@
-﻿namespace BlazorHybridDesktop
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorHybridDesktop
 {
     public partial class Main
     {
+        [Inject] private CounterState CounterState { get; set; }
+
         protected override void OnInitialized() => CounterState.StateChanged += StateHasChanged;
 
         public void Dispose() => CounterState.StateChanged -= StateHasChanged;
