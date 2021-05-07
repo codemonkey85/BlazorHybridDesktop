9c95a4d99ea92d6ea4d615610660aa663998f578
Michael Bond <codemonkey8510@icloud.com>
2021-05-07 19:17:53 -0400

    Fix web UI

---
+++ BlazorHybridDesktop/WebUI/IndexComponent.razor.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorHybridDesktop.WebUI
+{
+    public partial class IndexComponent
+    {
+        [Inject] private TodosState TodosState { get; set; }
+
+        [Inject] private HttpClient HttpClient { get; set; }
+
+        protected override async void OnInitialized()
+        {
+            TodosState.Todos = await HttpClient.GetAllTodos();
+            StateHasChanged();
+        }
+    }
+}
--- BlazorHybridDesktop/WebUI/Pages/Index.razor.cs
+++ BlazorHybridDesktop/WebUI/Pages/Index.razor.cs
@@ -1,12 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
-using System.Net.Http;
-
-namespace BlazorHybridDesktop.WebUI.Pages
+﻿namespace BlazorHybridDesktop.WebUI.Pages
 {
     public partial class Index
     {
-        [Inject] private TodosState TodosState { get; set; }
-
-        [Inject] private HttpClient HttpClient { get; set; }
     }
 }
